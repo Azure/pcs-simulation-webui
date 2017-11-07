cd42d748106b9b887806f7455f1d1fcb61403eb3
Stephen Pryor <stpryor@microsoft.com>
2017-11-07 13:45:00 -0800

    Create base application

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom'; 
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="app">
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
