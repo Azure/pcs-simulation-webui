65c0265efc5b9a4a702aee77569c077797625bc5
Hugh Xiong <maple10001@users.noreply.github.com>
2017-11-15 10:13:10 -0800

    add select component

--- src/components/shared/forms/forms.js
+++ src/components/shared/forms/forms.js
@@ -4,7 +4,11 @@ import React, { Component } from 'react';
 
 import { Svg } from '../svg/svg';
 import { svgs, isFunc } from 'utilities';
+<<<<<<< HEAD
 import { IotSelect } from '../iotSelect/iotSelect';
+=======
+import PcsSelect from '../pcsSelect/pcsSelect';
+>>>>>>> add select component
 
 import './forms.css';
 
@@ -58,7 +62,11 @@ export const FormControl = (props) => {
     case 'duration':
       return <DurationControl {...controlProps} />;
     case 'select':
+<<<<<<< HEAD
       return <IotSelect {...controlProps} />;
+=======
+      return <PcsSelect {...controlProps} />;
+>>>>>>> add select component
     default:
       return null; // Unknown form control
   }
