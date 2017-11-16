4888924285793fcd39e0d5cb465129040222a976
Hugh Xiong <maple10001@users.noreply.github.com>
2017-11-15 16:24:50 -0800

    remove unused variable

--- src/components/shared/iotSelect/iotSelect.js
+++ src/components/shared/iotSelect/iotSelect.js
@@ -1,6 +1,5 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-<<<<<<< HEAD
 import React from 'react';
 import Select from 'react-select';
 import { joinClasses } from 'utilities';
@@ -8,11 +7,3 @@ import { joinClasses } from 'utilities';
 import './iotSelect.css';
 
 export const IotSelect = ({className, ...props}) => <Select className={joinClasses('iot-select', className)} {...props} />;
-=======
-import React, { Component } from 'react';
-import Select from 'react-select';
-
-import './iotSelect.css';
-
-export const IotSelect = (props) => <Select {...props} />;
->>>>>>> add theme to select input
