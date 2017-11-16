96199ffc34f00fe4ad661e20cecadcf60cd25f30
Stephen Pryor <stpryor@microsoft.com>
2017-11-16 13:47:06 -0800

    Fix SVG issue in the tests

--- src/components/shared/svg/svg.js
+++ src/components/shared/svg/svg.js
@@ -6,8 +6,14 @@ import ReactSVG from 'react-svg';
 import './svg.css';
 
 /** Wraps an svg in a bounding box container for easier styling */
-export const Svg = ({ className, onClick, ...props } = {}) => (
-  <div className={`svg-container ${className || ''}`} onClick={onClick}>
-    <ReactSVG {...props} className={props.svgClassName} />
-  </div>
-);
+export const Svg = ({ className, onClick, ...props } = {}) => {
+  // ReactSVG will cause an error in the tests because Jest JSDOM doesn't
+  // support SVGs. This check will prevent the component from trying to
+  // render an SVG if it isn't supported
+  const svgsSupported = window && window.SVGSVGElement;
+  return (
+    <div className={`svg-container ${className || ''}`} onClick={onClick}>
+      { svgsSupported && <ReactSVG {...props} className={props.svgClassName} /> }
+    </div>
+  );
+};
