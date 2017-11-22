cf8e974998f2d7fae0182f90e466a9db99c5fad9
Stephen Pryor <stpryor@microsoft.com>
2017-11-22 13:14:54 -0800

    Fix failing test

--- src/components/pages/simulation/simulation.test.js
+++ src/components/pages/simulation/simulation.test.js
@@ -9,8 +9,19 @@ import 'polyfills';
 
 describe('Simulation Component', () => {
   let wrapper;
-
+  const mockProps = {
+    simulation: {
+      eTag: `"2600c470-0000-0000-0000-5a15e4d60000"`,
+      enabled: false,
+      startTime: '17-11-22T00:15:58+00:00',
+      endTime: '2017-11-29T00:15:58+00:00',
+      id: '1',
+      deviceModels: [
+        { Id: 'chiller-01', Count: 100 }
+      ]
+    }
+  };
   it('Renders without crashing', () => {
-    wrapper = mount(<Simulation />);
+    wrapper = mount(<Simulation {...mockProps} />);
   });
 });
