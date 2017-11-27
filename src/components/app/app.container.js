0dce384e200d0ac7e2d5859548f1e02dee0a6f76
Stephen Pryor <stpryor@microsoft.com>
2017-11-27 10:18:33 -0800

    Minor refactoring (#32)
    * Minor refactoring
    
    * Update comment

--- src/components/app/app.container.js
+++ src/components/app/app.container.js
@@ -2,9 +2,14 @@
 
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { routeEvent } from 'actions';
 import App from './app';
 
-// Wrap with the router and connect (to access dispatch)
-const AppContainer = withRouter(connect()(App));
+// Wrap with the router and wrap the dispatch method
+const mapDispatchToProps = dispatch => ({
+  registerRouteEvent: pathname => dispatch(routeEvent(pathname))
+});
+
+const AppContainer = withRouter(connect(undefined, mapDispatchToProps)(App));
 
 export default AppContainer;
