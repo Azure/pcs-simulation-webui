e25ea427baea664bcc205f9bf4daf48a17c031e5
Stephen Pryor <stpryor@microsoft.com>
2017-12-04 12:24:29 -0800

    Address some minor UI issues (#50)
    * Address some minor UI issues
    
    * Fix bug
    
    * Refactor method
    
    * Fix font issue in inputs with radio icons

--- src/components/app/app.container.js
+++ src/components/app/app.container.js
@@ -2,12 +2,14 @@
 
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { AuthService } from 'services';
 import { epics as appEpics } from 'store/reducers/appReducer';
 import App from './app';
 
 // Wrap with the router and wrap the dispatch method
 const mapDispatchToProps = dispatch => ({
-  registerRouteEvent: pathname => dispatch(appEpics.detectRouteChange.action(pathname))
+  registerRouteEvent: pathname => dispatch(appEpics.detectRouteChange.action(pathname)),
+  logout: () => AuthService.logout()
 });
 
 const AppContainer = withRouter(connect(undefined, mapDispatchToProps)(App));
