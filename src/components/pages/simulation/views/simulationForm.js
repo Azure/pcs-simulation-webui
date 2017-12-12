2d930722dad6104dbdda80713149d4c92d606efc
Hugh Xiong <maple10001@users.noreply.github.com>
2017-12-12 15:03:02 -0800

    Clear sensors state when device model is not `Custom` (#64)
    * clear sensors
    
    * check value instead of label

--- src/components/pages/simulation/views/simulationForm.js
+++ src/components/pages/simulation/views/simulationForm.js
@@ -211,7 +211,7 @@ class SimulationForm extends LinkedComponent {
     const deviceModels = [{
       id: deviceModel.value,
       count: numDevices,
-      sensors,
+      sensors: deviceModel.value === 'Custom' ? sensors : [],
       ...telemetryFrequency
     }];
     const modelUpdates = {
