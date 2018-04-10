ebb99cdc0216ff0a400a78d3b2e1bfb4e3a1a2ad
avanikp <32693971+avanikp@users.noreply.github.com>
2018-04-10 16:30:36 -0700

    set checkbox to default checked, rename opt in prop

--- src/components/app/header/settings.js
+++ src/components/app/header/settings.js
@@ -9,28 +9,33 @@ class Settings extends Component {
 
   constructor(props) {
     super(props);
-
+    
     this.state = { 
-      consentCheckboxChecked: this.props.dataInsightsConsent === "true",
+      diagnosticsOptOutCheckboxChecked: !((this.props || {}).settings || {}).diagnosticsOptOut
     };
   }
 
+  componentWillReceiveProps(nextProps) {
+        const { settings: {diagnosticsOptOut} } = nextProps;
+        this.setState({ diagnosticsOptOutCheckboxChecked: !diagnosticsOptOut });
+  }
+
   toggleCheckbox = () => {
-    this.setState({ consentCheckboxChecked: !this.state.consentCheckboxChecked }, 
+    this.setState({ diagnosticsOptOutCheckboxChecked: !this.state.diagnosticsOptOutCheckboxChecked }, 
         () => this.props.updateSolutionSettings({
             ...this.props.settings,
-            dataInsightsConsent: this.state.consentCheckboxChecked
-    }));
-  };
+            diagnosticsOptOut: !this.state.diagnosticsOptOutCheckboxChecked
+        })
+      ); 
+    };
 
   render() {
-    const {dataInsightsConsent} = this.props;
     return (
         <div className="consent">
-            <h2 className="dropdown-item">{ this.props.t('header.dataInsightsConsentHeader') }</h2>
-            <label className="dropdown-item">{ this.props.t('header.dataInsightsConsentText') }</label><br />
-            <input type="checkbox" className="dropdown-item" checked={this.state.consentCheckboxChecked} onChange={this.toggleCheckbox}/>
-            <label className="dropdown-item">{ this.props.t('header.dataInsightsConsentCheckbox') }</label>
+            <h2 className="dropdown-item">{ this.props.t('header.sendDiagnosticsHeader') }</h2>
+            <label className="dropdown-item">{ this.props.t('header.sendDiagnosticsText') }</label><br/><br/>
+            <input type="checkbox" className="dropdown-item" checked={this.state.diagnosticsOptOutCheckboxChecked} onChange={this.toggleCheckbox}/>
+            <label className="dropdown-item">{ this.props.t('header.sendDiagnosticsCheckbox') }</label>
         </div>
     );
   }
