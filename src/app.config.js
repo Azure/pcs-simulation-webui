5a23909834f36ac7b26b1f0184452649220712cd
REDMOND\v-huixio <maple10001@foxmail.com>
2018-04-25 16:48:18 -0700

    Update config file
    1, Add deviceModelsApiUr.
    2, Add pagenation size

--- src/app.config.js
+++ src/app.config.js
@@ -5,7 +5,7 @@ const Config = {
   simulationApiUrl: '/devicesimulation-svc/v1/',
   diagnosticsApiUrl: '/diagnostics-svc/v1/',
   configApiUrl: '/config-svc/v1/',
-
+  deviceModelsApiUrl: '/devicesimulation-svc/v2/deviceModels/',
 
   // Constants
   retryWaitTime: 2000, // On retryable error, retry after 2s
@@ -14,7 +14,8 @@ const Config = {
   maxSimulatedDevices: 20000,
   simulationStatusPollingInterval: 10000, // 10s
   customSensorValue: 'custom',
-  defaultAjaxTimeout: 10000, // 10s
+  defaultAjaxTimeout: 10000, // 10s,
+  paginationPageSize: 15
 };
 
 export default Config;
