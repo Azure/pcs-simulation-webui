46631a3c9eefd108f9e7778df963a1c2a5c7efc6
Hugh Xiong <maple10001@users.noreply.github.com>
2018-08-13 13:19:34 -0700

    Apply formatter rules

--- src/components/pages/deviceModels/flyouts/views/deviceModelUploadForm.js
+++ src/components/pages/deviceModels/flyouts/views/deviceModelUploadForm.js
@@ -16,13 +16,13 @@ import {
   Indicator
 } from 'components/shared';
 
-import './deviceModelForm.css'
+import './deviceModelForm.css';
 
 const initialFormState = {
   deviceModel: undefined,
   scripts: [],
   fileMismatching: false
-}
+};
 
 class DeviceModelUploadForm extends Component {
   constructor(props) {
@@ -37,30 +37,31 @@ class DeviceModelUploadForm extends Component {
   componentDidUpdate(prevProps, prevState) {
     const { scripts } = this.state;
 
-    if (scripts.length > 0 && scripts.some(({ validationResult }) => !validationResult )) {
+    if (scripts.length > 0 && scripts.some(({ validationResult }) => !validationResult)) {
       Observable.from(scripts)
         .filter(({ validation = {} }) => !validation.success)
-        .flatMap(({ file }) => SimulationScriptsService
-            .validateSimulationScript(file)
-            .map(validationResult => ({file, validationResult}))
+        .flatMap(({ file }) =>
+          SimulationScriptsService.validateSimulationScript(file)
+            .map(validationResult => ({ file, validationResult }))
             .catch(error => {
-              return Observable.of({file, validationResult: {
-                success: false,
-                messages: [error.ajaxError.response.Messages]
-              }});
+              return Observable.of({
+                file,
+                validationResult: {
+                  success: false,
+                  messages: [error.ajaxError.response.Messages]
+                }
+              });
             })
         )
-        .reduce((scripts, script) => ([...scripts, script]), [])
-        .subscribe(
-          scripts => {
-            this.setState({
-              scripts: [
-                ...this.state.scripts.filter(({validationResult}) => (validationResult || {}).success),
-                ...scripts
-              ]
-            });
-          }
-        )
+        .reduce((scripts, script) => [...scripts, script], [])
+        .subscribe(scripts => {
+          this.setState({
+            scripts: [
+              ...this.state.scripts.filter(({ validationResult }) => (validationResult || {}).success),
+              ...scripts
+            ]
+          });
+        });
     }
   }
 
@@ -69,7 +70,7 @@ class DeviceModelUploadForm extends Component {
     for (let i = 0; i < value.files.length; i++) files.push(value.files[i]);
     const result = this.filesSanityCheck(files);
     this.setState({ ...initialFormState, fileMismatching: !result });
-  }
+  };
 
   filesSanityCheck = (files = []) => {
     if (files.length === 0) return false;
@@ -77,8 +78,8 @@ class DeviceModelUploadForm extends Component {
 
     // Allow one json file only per device model
     if (jsonFiles.length !== 1) return false;
-    const [ jsonFile ] = jsonFiles;
-    const scripts =  files.filter(file => file.type === 'application/javascript');
+    const [jsonFile] = jsonFiles;
+    const scripts = files.filter(file => file.type === 'application/javascript');
     // Require at least one js file from user
     if (scripts.length < 1) return false;
 
@@ -88,7 +89,7 @@ class DeviceModelUploadForm extends Component {
     this.readFileAsText(jsonFile)
       .then(content => {
         const deviceModel = JSON.parse(content);
-        const { CloudToDeviceMethods = {} , Simulation: { Scripts } = { Scripts: [] } } = deviceModel;
+        const { CloudToDeviceMethods = {}, Simulation: { Scripts } = { Scripts: [] } } = deviceModel;
         const requiredScripts = [...Object.values(CloudToDeviceMethods), ...Scripts];
         const requiredScriptNames = new Set(requiredScripts.map(({ Path }) => Path));
         if (!isEqual(requiredScriptNames, uploadedScriptNames)) {
@@ -98,7 +99,10 @@ class DeviceModelUploadForm extends Component {
         } else {
           this.setState({
             deviceModel,
-            scripts: scripts.map(file => ({ file, validationResult: undefined }))
+            scripts: scripts.map(file => ({
+              file,
+              validationResult: undefined
+            }))
           });
         }
       })
@@ -106,7 +110,7 @@ class DeviceModelUploadForm extends Component {
         // TODO: show error message to user
         console.warn(e.message);
       });
-  }
+  };
 
   readFileAsText = file => {
     const reader = new FileReader();
@@ -114,35 +118,35 @@ class DeviceModelUploadForm extends Component {
     return new Promise((resolve, reject) => {
       reader.onerror = () => {
         reader.abort();
-        reject(new DOMException("Problem parsing input file."));
+        reject(new DOMException('Problem parsing input file.'));
       };
 
       reader.onload = () => {
         resolve(reader.result);
-      }
+      };
 
       reader.readAsText(file);
     });
-  }
+  };
 
   formIsValid() {
     const { deviceModel, scripts } = this.state;
     return !isEmpty(deviceModel) && scripts.every(({ validationResult }) => (validationResult || {}).success);
   }
 
-  apply = (event) => {
+  apply = event => {
     event.preventDefault();
     const { deviceModel, scripts } = this.state;
 
     // Uploading scripts
     Observable.from(scripts)
-      .flatMap(({file}) => SimulationScriptsService
-          .uploadsSimulationScript(file)
-          .catch(error => console.log(error))
-      )
-      .reduce((scripts, script) => ({...scripts, [script.name]: script}), {})
+      .flatMap(({ file }) => SimulationScriptsService.uploadsSimulationScript(file).catch(error => console.log(error)))
+      .reduce((scripts, script) => ({ ...scripts, [script.name]: script }), {})
       .map(scripts => {
-        const { CloudToDeviceMethods = {} , Simulation: { Scripts } } = deviceModel;
+        const {
+          CloudToDeviceMethods = {},
+          Simulation: { Scripts }
+        } = deviceModel;
 
         // Mapping methods with storage id
         const methods = Object.keys(CloudToDeviceMethods)
@@ -157,7 +161,7 @@ class DeviceModelUploadForm extends Component {
               }
             };
           })
-          .reduce((methods, method) => ({...methods, ...method}), {});
+          .reduce((methods, method) => ({ ...methods, ...method }), {});
 
         // Mapping behavior scripts with storage id
         const simulationScripts = Scripts.map(({ Path }) => ({
@@ -179,11 +183,9 @@ class DeviceModelUploadForm extends Component {
         // Create the device model in storage
         return this.props.uploadDeviceModel(model);
       })
-      .subscribe(
-        response => {
-          // TODO: Decide to show success message or not
-        }
-      )
+      .subscribe(response => {
+        // TODO: Decide to show success message or not
+      });
   };
 
   clearAll = () => {
@@ -191,15 +193,12 @@ class DeviceModelUploadForm extends Component {
     this.setState({ ...initialFormState, formVersion });
   };
 
-  render () {
+  render() {
     const { t } = this.props;
     const { deviceModel, scripts, changesApplied, formVersion, fileMismatching } = this.state;
     const showError = fileMismatching && !deviceModel && scripts.length === 0;
     return (
-      <form
-        key={`device-model-form-${formVersion}`}
-        onSubmit={this.apply}
-        className='device-model-form-container'>
+      <form key={`device-model-form-${formVersion}`} onSubmit={this.apply} className="device-model-form-container">
         <FormSection>
           <FormLabel>{t('deviceModels.flyouts.upload.create')}</FormLabel>
         </FormSection>
@@ -213,68 +212,53 @@ class DeviceModelUploadForm extends Component {
               name="uploader"
               accept=".json, .js"
               multiple
-              onChange={this.uploadFiles} />
-            <label htmlFor="fileUpload">
-              { t('deviceModels.flyouts.upload.browse') }
-            </label>
+              onChange={this.uploadFiles}
+            />
+            <label htmlFor="fileUpload">{t('deviceModels.flyouts.upload.browse')}</label>
           </div>
         </FormSection>
         <FormSection>
-        {
-          this.state.deviceModel &&
-          <FormGroup>
-            <FormLabel>{t('deviceModels.flyouts.new.name')}</FormLabel>
-            <div>{deviceModel.Name}</div>
-          </FormGroup>
-        }
-        {
-          scripts.length > 0 &&
-          <FormGroup>
-            <FormLabel>
-              { t('deviceModels.flyouts.upload.uploadedFiles') }
-            </FormLabel>
-            {
-              this.state.scripts
+          {this.state.deviceModel && (
+            <FormGroup>
+              <FormLabel>{t('deviceModels.flyouts.new.name')}</FormLabel>
+              <div>{deviceModel.Name}</div>
+            </FormGroup>
+          )}
+          {scripts.length > 0 && (
+            <FormGroup>
+              <FormLabel>{t('deviceModels.flyouts.upload.uploadedFiles')}</FormLabel>
+              {this.state.scripts
                 .sort((a, b) => a.file.name.localeCompare(b.file.name))
-                .map(({ file, validationResult = {} }, idx) =>
+                .map(({ file, validationResult = {} }, idx) => (
                   <div key={`script-${idx}`} className="upload-results-container">
                     <div className="file-name">{file.name}</div>
                     <div className="validation-result">
-                    {
-                      validationResult.success === undefined
-                      ? <Indicator size='mini' />
-                      : validationResult.success ? '\u2714' : '\u2716'
-                    }
+                      {validationResult.success === undefined ? (
+                        <Indicator size="mini" />
+                      ) : validationResult.success ? (
+                        '\u2714'
+                      ) : (
+                        '\u2716'
+                      )}
                     </div>
                     <div className="validation-message">
-                    {
-                      (validationResult.messages || []).map((error, idx) =>
-                        <ErrorMsg key={`script-error-${idx}`}>
-                          { error }
-                        </ErrorMsg>)
-                    }
+                      {(validationResult.messages || []).map((error, idx) => (
+                        <ErrorMsg key={`script-error-${idx}`}>{error}</ErrorMsg>
+                      ))}
                     </div>
                   </div>
-                )
-            }
-          </FormGroup>
-        }
+                ))}
+            </FormGroup>
+          )}
         </FormSection>
-        {
-          showError &&
-          <ErrorMsg>{ t('deviceModels.flyouts.upload.errorMsg') }</ErrorMsg>
-        }
+        {showError && <ErrorMsg>{t('deviceModels.flyouts.upload.errorMsg')}</ErrorMsg>}
         <FormActions>
           <BtnToolbar>
-            <Btn
-              disabled={!this.formIsValid() || changesApplied}
-              type="submit">
-              { t('deviceModels.flyouts.save') }
+            <Btn disabled={!this.formIsValid() || changesApplied} type="submit">
+              {t('deviceModels.flyouts.save')}
             </Btn>
-            <Btn
-              svg={svgs.cancelX}
-              onClick={this.clearAll}>
-              { t('deviceModels.flyouts.clearAll') }
+            <Btn svg={svgs.cancelX} onClick={this.clearAll}>
+              {t('deviceModels.flyouts.clearAll')}
             </Btn>
           </BtnToolbar>
         </FormActions>
