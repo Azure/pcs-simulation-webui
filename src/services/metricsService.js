712d95b3f1495c79576bf412ecaf1a3908b3e28a
Hugh Xiong <maple10001@users.noreply.github.com>
2018-09-11 16:53:58 -0700

    Add iothub metrics service

---
+++ src/services/metricsService.js
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from './httpClient';
+import Config from 'app.config';
+import { toIothubMetricsModel } from './models';
+
+const ENDPOINT = Config.simulationApiUrl;
+
+//Config.diagnosticsApiUrl;
+
+/** Contains methods for calling the diagnostics service */
+export class MetricsService {
+  /**Log event */
+  static fetchIothubMetrics(id, payload = null) {
+    return HttpClient.post(
+      `${ENDPOINT}simulations/${id}/metrics/iothub`,
+      payload,
+      true
+    )
+    .map(toIothubMetricsModel)
+    .catch(error => Observable.throw(error));
+  }
+}
