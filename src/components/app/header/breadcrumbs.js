7fc894aeeb3a087fa7b78fe1b83f77f80d07546f
Hugh Xiong <maple10001@users.noreply.github.com>
2018-11-01 14:36:09 -0700

    Update src/components/app/header/breadcrumbs.js

--- src/components/app/header/breadcrumbs.js
+++ src/components/app/header/breadcrumbs.js
@@ -6,7 +6,7 @@ import { Route, Switch, NavLink } from 'react-router-dom';
 import { Svg } from 'components/shared';
 import { svgs, isDef } from 'utilities';
 
-const Crumb = ({ children }) => <div className="crumb">{children}</div>
+const Crumb = ({ children }) => <div className="crumb">{ children }</div>
 const Chevron = () => <Svg path={svgs.chevronRight} className="chevron-icon" />;
 
 const CrumbFromConfig = ({ t, crumb, match, isLast }) => {
@@ -15,12 +15,12 @@ const CrumbFromConfig = ({ t, crumb, match, isLast }) => {
 
   if (isDef(crumb.labelId) && !isDef(crumb.matchParam)) {
     if (!isLast) {
-      return [<NavLink to={crumb.to} className="crumb" key={crumb.to}>{t(crumb.labelId)}</NavLink>, separator]
+      return [<NavLink to={crumb.to} className="crumb" key={crumb.to}>{ t(crumb.labelId) }</NavLink>, separator]
     } else {
       return <Crumb to={crumb.to} key={crumb.to}>{t(crumb.labelId)}</Crumb>
     }
   } else if (!isDef(crumb.labelId) && isDef(crumb.matchParam)) {
-    return <Crumb to={crumb.to} key={crumb.to}>{match.params[crumb.matchParam]}</Crumb>
+    return <Crumb to={crumb.to} key={crumb.to}>{ match.params[crumb.matchParam] }</Crumb>
   }
 }
 
@@ -28,9 +28,20 @@ export const Breadcrumbs = ({ t, crumbsConfig }) => (
   <Switch>
     {
       crumbsConfig.map(({ path, crumbs }) =>
-        <Route key={path} exact path={path} render={props => {
-          return crumbs.map((crumb, idx) => <CrumbFromConfig {...props} key={crumb.to} t={t} crumb={crumb} isLast={idx === crumbs.length - 1} />);
-        }} />
+        <Route
+          key={path}
+          exact
+          path={path}
+          render={props =>
+            crumbs.map((crumb, idx) =>
+              <CrumbFromConfig
+                {...props}
+                key={crumb.to}
+                t={t}
+                crumb={crumb}
+                isLast={idx === crumbs.length - 1} />
+            )
+        } />
       )
     }
   </Switch>
